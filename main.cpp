ea4b7bdd9e32715388da55fbc65a31b092f8b56c
Matheus <matheuscruz500@gmail.com>
2024-03-30 00:39:30 -0300

    mais rabisco, ovo dormir

--- main.cpp
+++ main.cpp
@@ -1,22 +1,58 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <fstream>
-
-
+#include <vector>
+#include <iterator>
+#include <memory>
 #define COPY_BYTES_AND_ADVANCE_ITERATOR(iter, X) std::copy(iter, iter + sizeof(X), reinterpret_cast<uint8_t*>(&X)); iter += sizeof(X);
 
+typedef std::vector<uint8_t>::iterator buffer_iterator;
 
+enum class ConstantPoolTag : uint8_t {
+    CONSTANT_Utf8 = 1,
+    CONSTANT_Integer = 3,
+    CONSTANT_Float = 4,
+    CONSTANT_Long = 5,
+    CONSTANT_Double = 6,
+    CONSTANT_Class = 7,
+    CONSTANT_String = 8,
+    CONSTANT_Fieldref = 9,
+    CONSTANT_Methodref = 10,
+    CONSTANT_InterfaceMethodref = 11,
+    CONSTANT_NameAndType = 12,
+    CONSTANT_MethodHandle = 15,
+    CONSTANT_MethodType = 16,
+    CONSTANT_InvokeDynamic = 18
+};
 
-union Constant_Pool_Entry{
-    struct Constant_Class_info{
-        uint8_t tag;
-        uint16_t name_index;
-    };
-    struct Constant_Fieldref_info{
-        uint8_t tag;
-        uint16_t class_index;
-        uint16_t name_and_type_index;
+struct Constant_Pool_Entry{
+    uint8_t tag;
+    union {
+        struct { //CONSTANT_Class_info, CONSTANT_NameAndType_info
+            uint16_t name_index;
+            uint16_t descriptor_index;  //CONSTANT_Class_info nao tem esse, mas n rola de ter 2 struct com name_index, paciencia
+
+        };
+        struct { // CONSTANT_Fieldref_info, CONSTANT_Methodref_info, CONSTANT_InterfaceMethodref_info
+            uint16_t class_index;
+            uint16_t name_and_type_index;
+        };
+        struct { //CONSTANT_String_info
+            uint16_t string_index;
+        };
+        struct{ //CONSTANT_Integer_info, CONSTANT_Float_info
+            uint32_t bytes;
+        };
+        struct{ //CONSTANT_Long_info, CONSTANT_Double_info
+            uint32_t high_bytes;
+            uint32_t low_bytes;
+        };
+        struct{
+            uint16_t  lenght;
+            uint8_t bytes_vec[]; //o vetor ta dando bo, vamo ter q ir de malloc msm
+        };
     };
     // ta faltando tudo, so uma ideia de como implementar
+
 };
 
 
@@ -29,7 +65,7 @@ struct cp_info{
 struct attribute_info{
     uint16_t attribute_name_index;
     uint32_t attribute_length;
-    std::vector<uint8_t> info;
+    std::vector<uint8_t> info; // em tese nesse caso aqui e byte, num sei
 };
 
 struct field_info{
@@ -47,8 +83,62 @@ struct method_info{
     std::vector<attribute_info> attributes;
 };
 
+Constant_Pool_Entry buildConstantPoolEntry(int tag, uint8_t info[]){
+        //ler os bytes de acordo com o tag
+    switch (static_cast<ConstantPoolTag>(tag)) {
+        case ConstantPoolTag::CONSTANT_Utf8:
+            std::cout << "Constante Utf8" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_Integer:
+            std::cout << "Constante Integer" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_Float:
+            std::cout << "Constante Float" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_Long:
+            std::cout << "Constante Long" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_Double:
+            std::cout << "Constante Double" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_Class:
+            std::cout << "Constante Class" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_String:
+            std::cout << "Constante String" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_Fieldref:
+            std::cout << "Constante Fieldref" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_Methodref:
+            std::cout << "Constante Methodref" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_InterfaceMethodref:
+            std::cout << "Constante InterfaceMethodref" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_NameAndType:
+            std::cout << "Constante NameAndType" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_MethodHandle:
+            std::cout << "Constante MethodHandle" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_MethodType:
+            std::cout << "Constante MethodType" << std::endl;
+            break;
+        case ConstantPoolTag::CONSTANT_InvokeDynamic:
+            std::cout << "Constante InvokeDynamic" << std::endl;
+            break;
+        default:
+            throw std::runtime_error("ConstantPoolEntry nao existe amigao");
+    }
+}
 
+void buildConstantPoolTable(buffer_iterator iter, int constant_pool_count,std::vector<cp_info> constant_pool){
+    uint8_t tag;
+    for(int i = 0; i<constant_pool_count-1; i++){
 
+    }
+}
 
 
 
@@ -101,6 +191,7 @@ int main(int argc, char* argv[]) {
 
     printf("%X\n",magic);
     printf("%d.%d",major_version,minor_version);
+
 
 
 
